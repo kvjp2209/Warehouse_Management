ece2a1a8b9cfdbded770df6d9ec52f144e1024ba
Nguyễn Thế Kiên <90679946+kvjp2209@users.noreply.github.com>
2023-08-14 22:48:37 +0700

    update DTI client

---
+++ Project_Prn231_Client/Web_Client/Data/RequestDTO/BatchRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Web_Client.Data.RequestDTO
+{
+    public class BatchRequestDTO
+    {
+        public long? BatchId { get; set; }
+        public long ProductId { get; set; }
+        public int? QuantityIn { get; set; }
+        public int? QuantityOut { get; set; }
+        public DateTime? ImportDate { get; set; }
+        public DateTime? ExportDate { get; set; }
+        public long SupplierId { get; set; }
+        public long WarehouseId { get; set; }
+    }
+}
---
+++ Project_Prn231_Client/Web_Client/Data/RequestDTO/OrderRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_Client.Data.RequestDTO
+{
+    public class OrderRequestDTO
+    {
+        public long? OrderId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public int? TotalAmount { get; set; }
+        public long EmployeeId { get; set; }
+    }
+}
