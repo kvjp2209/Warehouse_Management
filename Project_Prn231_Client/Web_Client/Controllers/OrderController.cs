0400cac32e282f5d125959c5440c89d84258bac0
misa.bui <Misa.Bui@avepoint.com>
2023-08-22 13:30:35 +0700

    Map UI for BE

---
+++ Project_Prn231_Client/Web_Client/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Web_Client.Data.RequestDTO;
+using Web_Client.Data.ResponseDTO;
+using Web_Client.Services;
+
+namespace Web_Client.Controllers
+{
+    public class OrderController : Controller
+    {
+        public OrderController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = new ClientService(HttpContext);
+            var response = await client.Get<List<OrderResponseDTO>>("http://localhost:5299/api/order");
+            return View(response);
+        }
+    }
+}
